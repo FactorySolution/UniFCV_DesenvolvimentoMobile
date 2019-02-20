c2cc194e6104672e9e7e2d17b6a0dacf3bd25ea9
André Luiz de Oliveira <andre.npd@hotmail.com>
2019-02-20 17:22:46 -0300

    Tratando eventos de componentes no Android

---
+++ Aula180219/app/src/main/java/br/edu/unifcv/myapplication/MainActivity.java
@@ -0,0 +1,26 @@
+package br.edu.unifcv.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnCalcular;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnCalcular = findViewById(R.id.btn_calcular);
+        btnCalcular.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
